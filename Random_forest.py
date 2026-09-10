eb3942e352ca28eee8bb1e6ed67c23654bc29985
alivatankhah1988-ui <alivatankhah1988@gmail.com>
2026-09-10 16:23:05 +0000

    implement Random_forest model and evaluate performance

--- Random_forest.py
+++ Random_forest.py
@@ -1,19 +1,24 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import (
+    accuracy_score, 
+    precision_score, 
+    recall_score, 
+    f1_score, 
+    confusion_matrix)
 
 #=======================
-# 1. Load dataset
+# 1. Load Data
 #=======================
-
-data=pd.read_csv("Diabet_dataset.csv",sep=";")
+data= pd.read_csv("Diabet_dataset.csv", sep=";")
 
 #Remove Completely empty columns
-data=data.dropna(axis=1, how="all")#
+data=data.dropna(axis=1, how="all")
 
 print("Dataset shape:  ", data.shape)
 print(data.head())
@@ -21,78 +26,79 @@ print(data.head())
 #=======================
 # 2. Define X and y
 #=======================
-X= data.drop("Result", axis=1)
-y= data["Result"]
+
+X= data.drop ("Result", axis=1)
+y= data ["Result"]
 
 #=======================
-# 3. Define Columns
+# 3. Define columns
 #=======================
 categorical_features= ["Gender"]
-numeric_features= ["Age", "FBS", "HbA1c", "TG", "Chol"]
+numerical_features= ["Age", "FBS", "HbA1c", "TG", "Chol"]
 
 #=======================
 # 4. Preprocessing
 #=======================
 preprocessor= ColumnTransformer(
-    transformers=[("cat", 
-                   OneHotEncoder(handle_unknown= "ignore"), 
-                   categorical_features),
-                  ("num", StandardScaler(),
-                   numeric_features)
-                   ])
+    transformers=[
+        ("cat",
+        OneHotEncoder(handle_unknown="ignore"), 
+                      categorical_features)],
+                      remainder="passthrough")
 
 #=======================
-# 5. Create KNN pipeline
+# 5. Create Random forest model
 #=======================
 
-knn_model= Pipeline(
+model= Pipeline(
     steps=[
         ("preprocessor", preprocessor),
-        ("classifier", KNeighborsClassifier(n_neighbors=5))]
-    )
+        ("classifier", RandomForestClassifier(random_state=42))
+    ]
+)
+
 
 #=======================
-# 6. Train/Test Split
+# 6. Train / Test Split
 #=======================
 
 X_train, X_test, y_train, y_test= train_test_split(
-    X, 
+    X,
     y,
     test_size=0.30,
     random_state=42,
     stratify=y
-    )
-
+)
 print("\nTraining set: ", X_train.shape)
 print("Testing set:    ", X_test.shape)
+
+
 #=======================
 # 7. Train the model
 #=======================
-knn_model.fit(X_train,y_train)
-
+model.fit(X_train, y_train)
 
 #=======================
 # 8. Prediction
 #=======================
-y_pred= knn_model.predict(X_test)
+y_pred= model.predict(X_test)
 
 #=======================
 # 9. Evaluation
 #=======================
 accuracy= accuracy_score(y_test, y_pred)
-Precision= precision_score(y_test, y_pred, pos_label="YES")
+precision= precision_score(y_test, y_pred, pos_label="YES")
 recall= recall_score(y_test, y_pred, pos_label="YES")
 f1= f1_score(y_test, y_pred, pos_label="YES")
-cm= confusion_matrix(y_test,y_pred, labels=["NO", "YES"])
+cm= confusion_matrix(y_test, y_pred, labels=["NO", "YES"])
 
+print ("Random Forest Results")
+print ("-------------------")
 
-print("KNN Results")
-print("-------------------")
 print("Accuracy:   ", round(accuracy*100,2), "%")
-print("Precision:  ", round(Precision*100,2), "%")
+print("Precision:  ", round(precision*100,2), "%")
 print("Recall:     ", round(recall*100,2), "%")
 print("F1_score:   ", round(f1*100,2), "%")
 
 print ("\nConfusion Matrix : ")
 print (cm)
-
